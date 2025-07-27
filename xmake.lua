0b41a0567cde0b16ddf3804b3a54f46f36497e68
greed-106 <ymj20040312@gmail.com>
2025-07-27 21:36:34 +0800

    first commit

---
+++ xmake.lua
@@ -0,0 +1,7 @@
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+
+target("codec-from-scratch")
+    set_kind("binary")
+    add_files("src/*.cpp")
+
