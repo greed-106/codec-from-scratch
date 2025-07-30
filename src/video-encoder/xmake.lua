c001981e4723c95a28181a28463cb397400e95f7
greed-106 <ymj20040312@gmail.com>
2025-07-30 16:45:33 +0800

    将头文件的添加移入顶级xmake.lua

--- src/video-encoder/xmake.lua
+++ src/video-encoder/xmake.lua
@@ -1,4 +1,3 @@
 target("video-encoder")
     set_kind("binary")
-    add_includedirs(path.join(os.projectdir(), "include", "core"), {public = true})
     add_files("*.cpp")
--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,16 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
+set_encodings("utf-8")
 if is_plat("windows") then
-    add_cxxflags("/utf-8")
     add_defines("NOMINMAX")
 end
 
 set_languages("cxx20")
 
+add_includedirs("include", {public = true})
+
 includes(
-    "src/video-encoder"
+    "src/video-encoder",
+    "src/jpeg-encoder"
 )
